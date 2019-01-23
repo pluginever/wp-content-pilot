3db46f9f4dec78ef53f4315f3102a97db028cbc3
princeahmed <israilahmed5@gmail.com>
2019-01-23 10:24:08 +0600

    status field added

--- views/menu/status.php
+++ views/menu/status.php
@@ -1,3 +1,78 @@
+<?php
+
+// don't call the file directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+global $wpdb;
+global $wp_version;
+
+$information = array();
+
+$information['wordpress_version'] = array(
+	'label' => __( 'WordPress version', 'wp-content-pilot' ),
+	'value' => ! empty( $wp_version ) ? $wp_version : '-',
+);
+
+$information['wpcp_version'] = array(
+	'label' => __( 'WP Content Pilot version', 'wp-content-pilot' ),
+	'value' => WPCP_VERSION,
+);
+
+$information['php_version'] = array(
+	'label' => __( 'PHP version', 'wp-content-pilot' ),
+	'value' => ! empty( PHP_VERSION ) ? PHP_VERSION : '-',
+);
+
+$information['mysql_version'] = array(
+	'label' => __( 'MySQL version', 'wp-content-pilot' ),
+	'value' => $wpdb->get_var( "SELECT VERSION() AS version" ),
+);
+
+$information['curl_version'] = array(
+	'label' => __( 'cURL version', 'wp-content-pilot' ),
+	'value' => function_exists( 'curl_version' ) ? curl_version()['version'] : '',
+);
+
+$information['curlssl_version'] = array(
+	'label' => __( 'cURL SSL version', 'wp-content-pilot' ),
+	'value' => function_exists( 'curl_version' ) ? curl_version()['ssl_version'] : '',
+);
+
+$information['cron_url'] = array(
+	'label' => __( 'WP-Cron url', 'wp-content-pilot' ),
+	'value' => site_url( 'wp-cron.php' ),
+);
+
+$information['docroot'] = array(
+	'label' => __( 'Document root', 'wp-content-pilot' ),
+	'value' => $_SERVER['DOCUMENT_ROOT'],
+);
+
+$information['server'] = array(
+	'label' => __( 'SERVER', 'wp-content-pilot' ),
+	'value' => $_SERVER['SERVER_SOFTWARE'],
+);
+
+$information['os'] = array(
+	'label' => __( 'Operating System', 'wp-content-pilot' ),
+	'value' => PHP_OS,
+);
+
+$information['maxexectime'] = array(
+	'label' => __( 'Maximum execution time', 'wp-content-pilot' ),
+	'value' => sprintf(__( '%s seconds', 'wp-content-pilot' ), ini_get('max_execution_time')),
+);
+
+$information['language'] = array(
+	'label' => __( 'language', 'wp-content-pilot' ),
+	'value' => get_bloginfo( 'language' ),
+);
+
+?>
+
+
 <div class="wrap" id="wpcp-page">
 
 	<h1 class="wp-heading-inline"> Status > WP Content Pilot </h1>
@@ -27,10 +102,12 @@
 
 		<tbody>
 
-		<tr>
-			<td>WordPress version</td>
-			<td>5.0.3</td>
-		</tr>
+		<?php foreach ( $information as $info ) { ?>
+			<tr>
+				<td><?php echo $info['label'] ?></td>
+				<td><?php echo $info['value'] ?></td>
+			</tr>
+		<?php } ?>
 
 		</tbody>
 	</table>
