a1c8dc0a1b166f9db4bfeb65ff4a4e7768cba5f8
Raju Ahmmed <rajuahmmedbd@gmail.com>
2019-05-26 13:42:04 +0600

    Maybe frequency fixed

--- includes/action-functions.php
+++ includes/action-functions.php
@@ -36,9 +36,9 @@ function wpcp_run_automatic_campaign() {
 			$posted       = wpcp_get_post_meta( $campaign_id, '_post_count', 0 );
 			$current_time = current_time( 'timestamp' );
 			$diff         = $current_time - $last_run;
-//			if ( $diff < $frequency ) {
-//				continue;
-//			}
+			if ( $diff < $frequency ) {
+				continue;
+			}
 
 			if ( $posted >= $target ) {
 				wpcp_disable_campaign( $campaign_id );
