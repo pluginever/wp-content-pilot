9efaf1c6863843823485b267417c682cd346d4a6
MD Sultan Nasir Uddin <manikdrmc@gmail.com>
2019-08-22 10:27:35 +0600

    Preparing release v1.0.7

--- wp-content-pilot.php
+++ wp-content-pilot.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Content Pilot
  * Plugin URI:  https://www.pluginever.com
  * Description: WP Content Pilot automatically posts contents from various sources based on the predefined keywords.
- * Version:     1.0.6
+ * Version:     1.0.7
  * Author:      pluginever
  * Author URI:  https://www.pluginever.com
  * Donate link: https://www.pluginever.com
@@ -46,7 +46,7 @@ final class ContentPilot {
 	 *
 	 * @var string
 	 */
-	protected $version = '1.0.6';
+	protected $version = '1.0.7';
 
 	/**
 	 * @since 1.0.0
