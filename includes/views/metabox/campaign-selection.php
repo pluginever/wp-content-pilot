9fae506ef13743c497d84a6a3097cfb69bafde06
MD Sultan Nasir Uddin <manikdrmc@gmail.com>
2020-01-30 19:17:05 +0600

    add filter

--- includes/views/metabox/campaign-selection.php
+++ includes/views/metabox/campaign-selection.php
@@ -1,137 +1,133 @@
 <?php
-$modules = array(
+$modules = apply_filters( 'wpcp_campaign_modules_list', array(
 	array(
-		'name' => 'Article',
+		'name'     => 'Article',
 		'keywords' => 'article, articles, blog, posts, news',
 		'disabled' => false,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/article.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/article.png' )
 	),
 	array(
-		'name' => 'Feed',
+		'name'     => 'Feed',
 		'keywords' => 'job',
 		'disabled' => false,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/feed.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/feed.png' )
 	),
 	array(
-		'name' => 'youtube',
+		'name'     => 'youtube',
 		'keywords' => 'job',
 		'disabled' => false,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/youtube.png')
-	), array(
-		'name' => 'Flickr',
-		'keywords' => 'job',
-		'disabled' => false,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/flickr.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/youtube.png' )
 	),
 	array(
-		'name' => 'Envato',
+		'name'     => 'Flickr',
 		'keywords' => 'job',
 		'disabled' => false,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/envato.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/flickr.png' )
 	),
 	array(
-		'name' => 'BestBuy',
+		'name'     => 'Envato',
+		'keywords' => 'job',
+		'disabled' => false,
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/envato.png' )
+	),
+	array(
+		'name'     => 'BestBuy',
 		'keywords' => 'bestbuy, affiliation, affiliate',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/bestbuy.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/bestbuy.png' )
 	),
 	array(
-		'name' => 'CareerJet',
+		'name'     => 'CareerJet',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/careerjet.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/careerjet.png' )
 	),
 	array(
-		'name' => 'ClickBank',
+		'name'     => 'ClickBank',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/clickbank.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/clickbank.png' )
 	),
 	array(
-		'name' => 'Craigslist',
+		'name'     => 'Craigslist',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/craigslist.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/craigslist.png' )
 	),
 	array(
-		'name' => 'Ebay',
+		'name'     => 'Ebay',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/ebay.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/ebay.png' )
 	),
 	array(
-		'name' => 'Ezine Articles',
+		'name'     => 'Ezine Articles',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/ezine.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/ezine.png' )
 	),
 	array(
-		'name' => 'FaceBook',
+		'name'     => 'FaceBook',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/facebook.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/facebook.png' )
 	),
 	array(
-		'name' => 'Instragram',
+		'name'     => 'Instragram',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/instragram.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/instragram.png' )
 	),
 	array(
-		'name' => 'Itunes',
+		'name'     => 'Itunes',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/itunes.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/itunes.png' )
 	),
 	array(
-		'name' => 'Pinterest',
+		'name'     => 'Pinterest',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/pinterest.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/pinterest.png' )
 	),
 	array(
-		'name' => 'Quora',
+		'name'     => 'Quora',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/quora.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/quora.png' )
 	),
 	array(
-		'name' => 'Reddit',
+		'name'     => 'Reddit',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/reddit.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/reddit.png' )
 	),
 	array(
-		'name' => 'SoundCloud',
+		'name'     => 'Twitter',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/soundcloud.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/twitter.png' )
 	),
 	array(
-		'name' => 'Twitter',
+		'name'     => 'SoundCloud',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/twitter.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/soundcloud.png' )
 	),
 	array(
-		'name' => 'SoundCloud',
+		'name'     => 'Walmart',
 		'keywords' => 'job',
 		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/soundcloud.png')
-	),
-	array(
-		'name' => 'Walmart',
-		'keywords' => 'job',
-		'disabled' => true,
-		'image' => esc_url(WPCP_ASSETS_URL.'/images/modules/walmart.png')
+		'image'    => esc_url( WPCP_ASSETS_URL . '/images/modules/walmart.png' )
 	)
-);
+) );
 ?>
 <style>
-	#wpcp-campaign-actions, #side-sortables{
+	#wpcp-campaign-actions, #side-sortables {
 		display: none;
 	}
-	#campaign-selection{
+
+	#campaign-selection {
 		overflow: hidden;
 		padding-bottom: 30px;
 	}
@@ -140,17 +136,18 @@ $modules = array(
 	<div class="wpcp-choose-campaign-items">
 		<?php
 		foreach ( $modules as $module ) {
-			$slug = sanitize_title($module['name']);
-			$disabled = $module['disabled'] ==  true ? " disabled='disabled' ": '';
-			$input = sprintf('<input type="radio" name="_campaign_type" id="%1$s" value="%2$s" %3$s required="required"/>', $slug, $slug, $disabled);
+			$slug       = sanitize_title( $module['name'] );
+			$disabled   = $module['disabled'] == true ? " disabled='disabled' " : '';
+			$input      = sprintf( '<input type="radio" name="_campaign_type" id="%1$s" value="%2$s" %3$s required="required"/>', $slug, $slug, $disabled );
 			$image_link = $module['image'];
-			$label = sprintf('<label for="%1$s"><img src="%2$s" alt="%3$s-module"></label>',$slug,  $image_link, $module['name']);
-			$wrap_class = $module['disabled'] == true ? 'disabled-item': '';
-			echo sprintf('<div class="wpcp-choose-campaign-item '.$wrap_class.'">'.$input.$label.'</div>');
+			$label      = sprintf( '<label for="%1$s"><img src="%2$s" alt="%3$s-module"></label>', $slug, $image_link, $module['name'] );
+			$wrap_class = $module['disabled'] == true ? 'disabled-item' : '';
+			echo sprintf( '<div class="wpcp-choose-campaign-item ' . $wrap_class . '">' . $input . $label . '</div>' );
 		}
 		?>
 	</div>
 	<p class="submit">
-		<input type="submit" name="submit" id="submit" class="button button-primary button-large" value="<?php _e('Submit', 'wp-content-pilot');?>">
+		<input type="submit" name="submit" id="submit" class="button button-primary button-large"
+		       value="<?php _e( 'Submit', 'wp-content-pilot' ); ?>">
 	</p>
 </div>
