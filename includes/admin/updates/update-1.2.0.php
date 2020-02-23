4e1a46a77ff79ecd42dbc211dcf5337651d5bb1d
MD Sultan Nasir Uddin <manikdrmc@gmail.com>
2020-02-23 14:17:29 +0600

    fix update script

--- includes/admin/updates/update-1.2.0.php
+++ includes/admin/updates/update-1.2.0.php
@@ -9,15 +9,16 @@ function content_pilot_update_1_2_0() {
 	$wpdb->query( "ALTER TABLE $wpdb->wpcp_links DROP COLUMN raw_content;");
 	$wpdb->query( "ALTER TABLE $wpdb->wpcp_links DROP COLUMN score;");
 	$wpdb->query( "ALTER TABLE $wpdb->wpcp_links DROP COLUMN gmt_date;");
-	$wpdb->query( "ALTER TABLE $wpdb->wpcp_links DROP COLUMN created_at;");
+	$wpdb->query( "ALTER TABLE $wpdb->wpcp_links DROP COLUMN data;");
 	$wpdb->query( "ALTER TABLE $wpdb->wpcp_links DROP COLUMN created_at;");
 	$wpdb->query( "ALTER TABLE $wpdb->wpcp_links DROP COLUMN updated_at;");
+
 
 	$wpdb->query( "ALTER TABLE $wpdb->wpcp_logs DROP COLUMN updated_at;");
 	$wpdb->query( "ALTER TABLE $wpdb->wpcp_logs DROP COLUMN keyword;");
 	$wpdb->query( "ALTER TABLE $wpdb->wpcp_logs DROP COLUMN log_level;");
 	$wpdb->query( "ALTER TABLE $wpdb->wpcp_logs DROP COLUMN message;");
-	$wpdb->query( "ALTER TABLE $wpdb->wpcp_logs ADD COLUMN level varchar(20) NOT NULL DEFAULT '' AFTER camp_id;");
+	$wpdb->query( "ALTER TABLE $wpdb->wpcp_logs ADD COLUMN level varchar(20) NOT NULL DEFAULT 'info' AFTER camp_id;");
 	$wpdb->query( "ALTER TABLE $wpdb->wpcp_logs ADD COLUMN message text DEFAULT '' AFTER camp_id;");
 }
 
