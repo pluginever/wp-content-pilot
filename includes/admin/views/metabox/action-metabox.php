ccf20d99383afd79efb86004e928e86ac9b1deb0
sudipto-me <sudipto004.ruet@gmail.com>
2020-03-08 11:31:21 +0600

    campaign target range updated

--- includes/admin/views/metabox/action-metabox.php
+++ includes/admin/views/metabox/action-metabox.php
@@ -17,13 +17,16 @@ $can_publish = true;
 ?>
 <div id="submitpost" class="submitbox wpcp-submitbox">
 	<?php
+
 	echo WPCP_HTML::range_input( array(
 		'name'    => '_campaign_target',
 		'label'   => __( 'Campaign Target', 'wp-content-pilot' ),
 		'default' => 5,
+		'max'     => is_plugin_active( 'wp-content-pilot-pro/wp-content-pilot-pro.php' ) ? 5000 : 500,
 		'attrs'   => array(
-			'required' => 'required'
+			'required' => 'required',
 		),
+
 	) );
 
 	echo WPCP_HTML::range_input( array(
@@ -52,39 +55,41 @@ $can_publish = true;
 
 	echo WPCP_HTML::switch_input( array(
 		'name'  => '_campaign_status',
-		'check'  => 'active',
+		'check' => 'active',
 		'label' => __( 'Campaign Status', 'wp-content-pilot' ),
 	) );
 
 	?>
 
-	<div id="major-publishing-actions">
+    <div id="major-publishing-actions">
 		<?php if ( ! empty( $_REQUEST['post'] ) ) { ?>
-			<div id="delete-action">
-				<a class="submitdelete deletion"
-				   href="<?php echo get_delete_post_link( esc_attr( $_REQUEST['post'] ) ) ?>"
-				   title="<?php _e( 'Move to Trash', 'wp-content-pilot' ); ?>">
-					<button type="button" class="button-link button-link-delete">
+            <div id="delete-action">
+                <a class="submitdelete deletion"
+                   href="<?php echo get_delete_post_link( esc_attr( $_REQUEST['post'] ) ) ?>"
+                   title="<?php _e( 'Move to Trash', 'wp-content-pilot' ); ?>">
+                    <button type="button" class="button-link button-link-delete">
 						<?php _e( 'Trash', 'wp-content-pilot' ); ?>
-					</button>
-				</a>
-			</div>
+                    </button>
+                </a>
+            </div>
 		<?php } ?>
 
 		<?php if ( ! empty( $_REQUEST['post'] ) ) { ?>
-			<a href="<?php echo esc_url( $reset_search_url ); ?>" class="button-link">
+            <a href="<?php echo esc_url( $reset_search_url ); ?>" class="button-link">
 				<?php _e( 'Reset', 'wp-content-pilot' ); ?>
-			</a>
+            </a>
 		<?php } ?>
-		<div id="publishing-action">
-			<input name="original_publish" type="hidden" id="original_publish" value="<?php esc_attr_e( 'Update', 'wp-content-pilot' ) ?>"/>
-			<div class="publishing-action-btn">
-				<input name="publish" type="submit" class="button button-primary button-large" id="publish" value="<?php esc_attr_e( 'Update Campaign', 'wp-content-pilot' ) ?>"/>
-			</div>
-		</div>
+        <div id="publishing-action">
+            <input name="original_publish" type="hidden" id="original_publish"
+                   value="<?php esc_attr_e( 'Update', 'wp-content-pilot' ) ?>"/>
+            <div class="publishing-action-btn">
+                <input name="publish" type="submit" class="button button-primary button-large" id="publish"
+                       value="<?php esc_attr_e( 'Update Campaign', 'wp-content-pilot' ) ?>"/>
+            </div>
+        </div>
 
-		<div class="clear"></div>
-		<input type="hidden" name="_campaign_type" value="<?php echo esc_attr( $campaign_type ); ?>">
-		<input type="hidden" name="hidden_post_status" id="hidden_post_status" value="publish"/>
-	</div>
+        <div class="clear"></div>
+        <input type="hidden" name="_campaign_type" value="<?php echo esc_attr( $campaign_type ); ?>">
+        <input type="hidden" name="hidden_post_status" id="hidden_post_status" value="publish"/>
+    </div>
 </div>
