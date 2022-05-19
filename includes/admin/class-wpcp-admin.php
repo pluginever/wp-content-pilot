aa447d18939533c6db96fd5d3dd988e26e3e61a4
Sudipto Shakhari <Sudipto004.ruet@gmail.com>
2022-05-19 12:33:40 +0600

    update suggested changes

--- includes/admin/class-wpcp-admin.php
+++ includes/admin/class-wpcp-admin.php
@@ -51,7 +51,6 @@ class WPCP_Admin {
 		$hook = 'edit.php?post_type=wp_content_pilot';
 		add_submenu_page( $hook, __( 'Status', 'wp-content-pilot' ), __( 'Status', 'wp-content-pilot' ), 'edit_others_posts', 'wpcp-status', array( $this, 'status_page' ) );
 		add_submenu_page( $hook, __( 'Logs', 'wp-content-pilot' ), __( 'Logs', 'wp-content-pilot' ), 'edit_others_posts', 'wpcp-logs', array( $this, 'logs_page' ) );
-		//add_submenu_page( $hook, __('Help','wp-content-pilot'), '<span style="color:orange;">Help</span>', 'edit_others_posts', 'wpcp-help', array( $this, 'help_page' ) );
 	}
 
 	/**
@@ -69,14 +68,6 @@ class WPCP_Admin {
 	public function logs_page() {
 		wpcp_get_views( 'page/logs-page.php' );
 	}
-
-	/**
-	 * Help Page
-	 * @since 1.2.0
-	 */
-//	public function help_page() {
-//		wpcp_get_views( 'page/help-page.php' );
-//	}
 
 	/**
 	 * @since 1.2.0
