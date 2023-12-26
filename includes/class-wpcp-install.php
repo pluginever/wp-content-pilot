dc7d4bf4ba51664b86f59b3377dbcb0c9827ff78
wpfresher <kawsar.ahmed198133@gmail.com>
2023-12-26 12:38:00 +0600

    Fix phpcs issues.

--- includes/class-wpcp-install.php
+++ includes/class-wpcp-install.php
@@ -1,38 +1,57 @@
 <?php
+/**
+ * WPCP Install.
+ *
+ * @package WP Content Pilot
+ * @since 1.0.0
+ */
+
+// Exit if accessed directly.
 defined( 'ABSPATH' ) || exit();
 
+/**
+ * WPCP_Install Class.
+ *
+ * @package WP Content Pilot
+ * @since 1.0.0
+ */
 class WPCP_Install {
 	/**
+	 * Activate.
+	 *
 	 * @since 1.2.0
+	 * @return void
 	 */
 	public static function activate() {
 		$current_db_version   = get_option( 'wpcp_db_version', null );
 		$current_wpcp_version = get_option( 'wpcp_version', null );
 		self::create_tables();
 		self::populate();
-		//save db version
+		// Save db version.
 		if ( is_null( $current_wpcp_version ) ) {
 			update_option( 'wpcp_version', WPCP_VERSION );
 		}
-		//save db version
+		// Save db version.
 		if ( is_null( $current_db_version ) ) {
 			update_option( 'wpcp_db_version', WPCP_VERSION );
 		}
-		//save install date
-		if ( false == get_option( 'wpcp_install_date' ) ) {
+		// Save install date.
+		if ( false === get_option( 'wpcp_install_date' ) ) {
 			update_option( 'wpcp_install_date', current_time( 'timestamp' ) );
 		}
-
 	}
 
 	/**
+	 * Create database tables.
+	 *
 	 * @since 1.2.0
+	 * @return void
 	 */
 	public static function create_tables() {
 		global $wpdb;
 		$wpdb->hide_errors();
 
-		$table_schema = [
+		$table_schema = array(
 			"CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}wpcp_links` (
                 `id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
                 `camp_id` INT(11) NOT NULL,
@@ -53,15 +72,18 @@ class WPCP_Install {
                 `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
                 PRIMARY KEY (`id`)
             )  CHARACTER SET utf8 COLLATE utf8_general_ci;",
-		];
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		);
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 		foreach ( $table_schema as $table ) {
 			dbDelta( $table );
 		}
 	}
 
 	/**
+	 * Populate data.
+	 *
 	 * @since 1.2.0
+	 * @return void
 	 */
 	public static function populate() {
 		$article_settings = wpcp_get_settings( 'wpcp_settings_article' );
@@ -78,7 +100,10 @@ class WPCP_Install {
 	}
 
 	/**
+	 * Plugin deactivation hook.
+	 *
 	 * @since 1.2.0
+	 * @return void
 	 */
 	public static function deactivate() {
 		wp_clear_scheduled_hook( 'wpcp_per_minute_scheduled_events' );
