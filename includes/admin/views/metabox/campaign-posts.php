6325da77ef20a2fa716c26b10c9da87824490e46
wpfresher <kawsar.ahmed198133@gmail.com>
2023-12-31 11:07:26 +0600

    Fix phpcs issues.

--- includes/admin/views/metabox/campaign-posts.php
+++ includes/admin/views/metabox/campaign-posts.php
@@ -1,18 +1,20 @@
 <?php
-$posts = get_posts( array(
-	'post_type'   => wpcp_get_post_meta( $campaign_id, '_post_type', 'post' ),
-	'numberposts' => 10,
-	'meta_key'    => '_campaign_id',
-	'meta_value'  => $campaign_id,
-	'post_status' => array( 'publish', 'private', 'draft', 'pending' ),
-) );
+$posts = get_posts(
+	array(
+		'post_type'   => wpcp_get_post_meta( $campaign_id, '_post_type', 'post' ),
+		'numberposts' => 10,
+		'meta_key'    => '_campaign_id', // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_key
+		'meta_value'  => $campaign_id, // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_value
+		'post_status' => array( 'publish', 'private', 'draft', 'pending' ),
+	)
+);
 
 if ( empty( $posts ) ) {
-	echo sprintf( '<p>%s</p>', __( 'No posts generated yet', 'wp-content-pilot' ) );
+	printf( '<p>%s</p>', esc_html__( 'No posts generated yet', 'wp-content-pilot' ) );
 } else {
 	echo '<ul class="wpcp-campaign-posts">';
 	foreach ( $posts as $post ) {
-		echo sprintf( '<li><a href="%s">%s</a></li>', get_the_permalink( $post ), get_the_title( $post ) );
+		printf( '<li><a href="%s">%s</a></li>', esc_url( get_the_permalink( $post ) ), esc_html( get_the_title( $post ) ) );
 	}
 
 	echo '</ul>';
