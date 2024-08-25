30e5895438352046cdb29f13fbb75fb23477485f
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2024-08-25 14:21:59 +0600

    Fix phpcs issue

--- uninstall.php
+++ uninstall.php
@@ -13,8 +13,8 @@ $wpcp_settings = get_option( 'wpcp_settings_misc', array() );
 if ( isset( $wpcp_settings['uninstall_on_delete'] ) && 'on' === $wpcp_settings['uninstall_on_delete'] ) {
 	global $wpdb;
 	// Remove all database tables.
-	$wpdb->query( "DROP TABLE IF EXISTS " . $wpdb->prefix . 'wpcp_links' );
-	$wpdb->query( "DROP TABLE IF EXISTS " . $wpdb->prefix . 'wpcp_logs' );
+	$wpdb->query( 'DROP TABLE IF EXISTS ' . $wpdb->prefix . 'wpcp_links' );
+	$wpdb->query( 'DROP TABLE IF EXISTS ' . $wpdb->prefix . 'wpcp_logs' );
 
 	/** Cleanup Cron Events */
 	wp_clear_scheduled_hook( 'wpcp_per_minute_scheduled_events' );
