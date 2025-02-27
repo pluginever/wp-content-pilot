ae588768ddc67e12031e337387a9cf0304ca3045
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2025-02-27 13:34:28 +0600

    Update headers

--- wp-content-pilot.php
+++ wp-content-pilot.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WP Content Pilot
  * Plugin URI:        https://wpcontentpilot.com
  * Description:       WP Content Pilot automatically posts contents from various sources based on the predefined keywords.
- * Version:           2.0.7
+ * Version:           2.0.8
  * Requires at least: 5.0
  * Requires PHP:      7.4
  * Author:            PluginEver
