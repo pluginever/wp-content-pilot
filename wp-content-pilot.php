a8342ae218edae9754a24d152493b69d5a46b10b
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2025-03-25 15:48:33 +0600

    Prepare Release v2.0.8

--- wp-content-pilot.php
+++ wp-content-pilot.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WP Content Pilot
  * Plugin URI:        https://wpcontentpilot.com
  * Description:       WP Content Pilot automatically posts contents from various sources based on the predefined keywords.
- * Version:           2.0.8
+ * Version:           2.0.9
  * Requires at least: 5.0
  * Requires PHP:      7.4
  * Author:            PluginEver
