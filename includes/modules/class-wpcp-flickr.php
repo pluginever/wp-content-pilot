50c56755c825271391a4705dc488e074c7d5667f
DAnn2012 <dannunziata@hotmail.it>
2025-11-23 22:21:46 +0100

    Update class-wpcp-flickr.php

--- includes/modules/class-wpcp-flickr.php
+++ includes/modules/class-wpcp-flickr.php
@@ -348,7 +348,7 @@ EOT;
 
 		$log_url = admin_url( '/edit.php?post_type=wp_content_pilot&page=wpcp-logs' );
 
-		return new WP_Error( 'campaign-error', __( sprintf( 'No flickr article generated check <a href="%s">log</a> for details.', $log_url ), 'wp-content-pilot' ) );
+		return new WP_Error( 'campaign-error', sprintf( __( 'No flickr article generated check <a href="%s">log</a> for details.', 'wp-content-pilot' ), $log_url ) );
 	}
 
 
