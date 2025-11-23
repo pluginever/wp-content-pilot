d855b46d3e10c813c030e825703804294f56032d
DAnn2012 <dannunziata@hotmail.it>
2025-11-23 22:24:17 +0100

    Update class-wpcp-youtube.php

--- includes/modules/class-wpcp-youtube.php
+++ includes/modules/class-wpcp-youtube.php
@@ -397,7 +397,7 @@ EOT;
 
 		$log_url = admin_url( '/edit.php?post_type=wp_content_pilot&page=wpcp-logs' );
 
-		return new WP_Error( 'campaign-error', __( sprintf( 'No youtube article generated check <a href="%s">log</a> for details.', $log_url ), 'wp-content-pilot' ) );
+		return new WP_Error( 'campaign-error', sprintf( __( 'No youtube article generated check <a href="%s">log</a> for details.', 'wp-content-pilot' ), $log_url ) );
 	}
 
 	/**
